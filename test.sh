72f3eb7559348f813bc072f0c104a707d888ff44
Ranzo J Taylor, III <36489926+Ranzo3@users.noreply.github.com>
2021-01-28 13:41:13 -0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
     sqlplus
       EOF with auto indent and one tab
+      EOF with auto indent after changing upper right to "Tabs 8"
   EOF with erase auto indent and one tab
 	EOF with hard tab
-  
+	EOF with hard tab from Editor
+
